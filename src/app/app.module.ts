b5d5943fa51f3087a7980a39edefbf9638d1e086
Milena <balletmila@gmail.com>
2018-08-12 22:18:31 +0200

    sample animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
@@ -14,7 +16,9 @@ import { routing } from './app.routing';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,60 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular/animations'
 
 @Component({
     selector: 'app-home',
-    template: `<h1>{{welcome}}</h1>`
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    animations:[
+        trigger('goals', [
+            transition('* => *',[
+                query(':enter', style({opacity:0}), {optional:true}),
+                query(':enter', stagger('300ms',[
+                    animate('.6s ease-in', keyframes([
+                        style({opacity:0, transform:'translateY(-75%)',offset:0}),
+                        style({opacity:.5, transform:'translateY(35px)',offset:.3}),
+                        style({opacity:1, transform:'translateY(0)',offset:1})
+                    ]))
+                ]), {optional:true}),
+                query(':leave', stagger('300ms',[
+                    animate('.6s ease-in', keyframes([
+                        style({opacity:1, transform:'translateY(0)',offset:0}),
+                        style({opacity:.5, transform:'translateY(35px)',offset:.3}),
+                        style({opacity:0, transform:'translateY(-75%)',offset:1})
+                    ]))
+                ]), {optional:true}),
+            ])
+        ])
+    ]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
     welcome : string;
+
+    counter: number=0;
+    btnTxt: string='dodaj stuff';
+    goalTxt: string='cos tam pierwsze';
+    goals=['sadsdasda','asdasd'];
+    
+
+    
     constructor(){
-        this.welcome = "Welcome to home page";
+        this.welcome = "Welcome to Mila's page";
     };
+
+    ngOnInit(){
+        this.counter=this.goals.length;
+    }
+    
+    removeItem(i){
+        // delete this.goals[i];
+        this.goals.splice(i,1);
+        this.counter=this.goals.length;
+    }
+
+    addItemToArray(){
+        this.goals.push(this.goalTxt);
+        this.goalTxt='';
+        this.counter=this.goals.length;
+    }
+
 };
